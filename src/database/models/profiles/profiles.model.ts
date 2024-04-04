6d9069855f1e28ff842b50becd3fc74ddaa13bb2
lenghak <125646707+Lenghak@users.noreply.github.com>
2024-04-04 14:46:45 +0700

    feat: \profile model

--- src/database/models/profiles/profiles.model.ts
+++ src/database/models/profiles/profiles.model.ts
@@ -1,4 +1,11 @@
-import { pgEnum, pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+import {
+  bigint,
+  pgEnum,
+  pgTable,
+  timestamp,
+  uuid,
+  varchar,
+} from "drizzle-orm/pg-core";
 
 export const sexEnum = pgEnum("sexEnum", ["MALE", "FEMALE", "RNTS"]);
 
@@ -9,10 +16,18 @@ export const profiles = pgTable("profiles", {
   last_name: varchar("last_name", { length: 255 }).notNull().default(""),
 
   image_url: varchar("image_url"),
+  birthday: timestamp("birthday"),
 
   bio: varchar("bio", { length: 1023 }),
   gender: varchar("gender", { length: 255 }),
   sex: sexEnum("sex").default("RNTS"),
+  following_count: bigint("following_count", { mode: "number" }).default(0),
+  follower_count: bigint("follower_count", { mode: "number" }).default(0),
+  public_post_count: bigint("public_post_count", { mode: "number" }).default(0),
+  private_post_count: bigint("private_post_count", { mode: "number" }).default(
+    0,
+  ),
+  draft_post_count: bigint("draft_post_count", { mode: "number" }).default(0),
 
   created_at: timestamp("created_at", { withTimezone: true })
     .notNull()
